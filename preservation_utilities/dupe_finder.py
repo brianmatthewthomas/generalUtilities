739f4e4b068742c3950acf9fff5c651072f6c622
brian <bthomas@tsl.texas.gov>
2022-06-02 10:40:34 -0500

    new tool, duplicate file finder

--- preservation_utilities/dupe_finder.py
+++ preservation_utilities/dupe_finder.py
@@ -1,5 +1,6 @@
 # use this tool to check a directory for duplicate files. It uses SHA256 to check rather than filename
 # if you have very large files this will take a while
+# it checks ALL files and gives and EXACT so make sure you can see system files since those are included
 import os
 import hashlib
 
@@ -16,25 +17,30 @@ def create_sha256(filename):
     return fixity
 
 hashdict = {}
-
+# input variables, use relative OR full filepaths
 tocrawl = input("directory to check for dupes: ")
 logger = input("name of log file for duplicates: ")
 
 print("checking directory for duplicates")
 dirname1 = ""
+# start crawling directory recursively
 for dirpath, dirnames, filenames in os.walk(tocrawl):
     for filename in filenames:
+        # print out directory name so users know where in the process it is
         if dirname1 != dirpath:
             dirname1 = dirpath
             print("checking",dirname1)
         filename = os.path.join(dirpath, filename)
+        # send file to be checksummed
         hashish = create_sha256(filename)
+        # check dictionary, add to list of files if checksum match exists, create new key/list pair if not
         if hashish in hashdict:
             hashdict[hashish].append(filename)
         else:
             hashdict[hashish] = [filename]
 print("checking done, writing results")
 log = open(logger, "a")
+# check dictionary for lists with more than one item and print the filenames to a log file if that is the case
 for key in hashdict:
     variable = hashdict[key]
     if len(variable) > 1:
