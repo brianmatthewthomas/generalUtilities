19c2bbeff9a77f94d06d82df1d5da4958864d778
brian <bthomas@tsl.texas.gov>
2024-10-01 14:39:04 -0500

    updated timestamp handling

--- search/transcription_listener.py
+++ search/transcription_listener.py
@@ -3,6 +3,7 @@ import os
 import time
 import datetime
 import sys
+import json
 '''
 # instructions to get this working
 # install python3
@@ -10,20 +11,16 @@ import sys
 # 1: install ffmpeg, ensure if this is windows that it is on the computer path by trying to invoke on the command line
 # 2: install/confirm install of pip on a windows computer
 # 3: run install instructions for whisper. should be: pip install -U openai-whisper
-
-For Gary testing
-download the mp3 file at https://tsl.access.preservica.com/uncategorized/IO_0948332a-d79a-4d23-b46d-5b1f76b64914/
-save to a folder containing just the recording
-invoke script and target the folder using an absolute filepath is possible
-it'll take a hot second to load the first time. note the start transcription timestamp and the end timestamp. There'll be no progress bar to advise status as it goes, just when it finishes.
-output will be a .srt file
-hit ctrl+c to stop/pause
 '''
 
 # set single parameter
 crawl_this = input("directory to crawl for mp3 files: ")
+while crawl_this.startswith("'") or crawl_this.startswith('"'):
+    crawl_this = crawl_this[1:]
+while crawl_this.endswith("'") or crawl_this.endswith('"'):
+    crawl_this = crawl_this[:-1]
 
-accept_list = ['mp3', 'MP3', 'mp4', 'MP4', 'wav', 'WAV']
+accept_list = ['mp3', 'MP3', 'mp4', 'MP4', 'wav', 'WAV', 'mov', 'MOV', 'avi', 'AVI']
 #load the base model
 print("loading transcription model")
 model = whisper.load_model("medium.en")
@@ -42,15 +39,23 @@ while flag is False:
                     transcription_filename = os.path.join(dirpath, transcription_filename)
                     if not os.path.isfile(transcription_filename):
                         print(f"starting transcription for {filename1} at {time.asctime()}")
-                        result = model.transcribe(filename1)
+                        result = model.transcribe(filename1, fp16=False)
                         print(f"transcription for {filename1} completed at {time.asctime()}")
                         transcription_string = ""
                         for block in result['segments']:
-                            start = str(datetime.timedelta(seconds=block['start']))[:-3]
-                            end = str(datetime.timedelta(seconds=block['end']))[:-3]
+                            start = str(datetime.timedelta(seconds=block['start']))
+                            end = str(datetime.timedelta(seconds=block['end']))
+                            if "." not in start:
+                                start = f"{start}.000000"
+                            if "." not in end:
+                                end = f"{end}.000000"
                             if transcription_string == "":
-                                start = "00:00:00.000"
-                                end = str(datetime.timedelta(seconds=result['segments'][1]['start']))[:-3]
+                                start = "00:00:00.000000"
+                                end = str(datetime.timedelta(seconds=result['segments'][1]['start']))
+                                if "." not in end:
+                                    end = f"{end}.000000"
+                            start = start[:-3]
+                            end = end[:-3]
                             while len(start) < 12:
                                 start = f"0{start}"
                             while len(end) < 12:
@@ -60,6 +65,10 @@ while flag is False:
                             transcription_string += f" {block['text']}\n\n"
                         with open(transcription_filename, "w") as w:
                             w.write(transcription_string)
+                        w.close()
+                        #to troubleshoot faulty output, create companion json file
+                        with open(f"{transcription_filename}.json", "w") as w:
+                            json.dump(result, w)
                         w.close()
                         print(f"{transcription_filename} created, pausing for 2 minutes for funsies")
                         time.sleep(120)
